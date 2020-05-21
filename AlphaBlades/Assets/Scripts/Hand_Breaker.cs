109f771b82020f856d4e687dc4feaf1ae773cc35
lucho1 <luchosuaya@hotmail.com>
2020-05-21 23:05:19 +0200

    Added Music

--- AlphaBlades/Assets/Scripts/Hand_Breaker.cs
+++ AlphaBlades/Assets/Scripts/Hand_Breaker.cs
@@ -6,11 +6,19 @@ public class Hand_Breaker : MonoBehaviour
 {
     public CubesBehaviour.CubeType Type;
     gameManager GM;
+    AudioSource boxHit;
+    AudioSource bladeHit;
 
     // Start is called before the first frame update
     void Start()
     {
         GM = GameObject.Find("gameManager").GetComponent<gameManager>();
+        boxHit = GameObject.Find("BoxHitSound").GetComponent<AudioSource>();
+
+        if(Type == CubesBehaviour.CubeType.Red)
+            bladeHit = GameObject.Find("RedBladeHit").GetComponent<AudioSource>();
+        else
+            bladeHit = GameObject.Find("BlueBladeHit").GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -36,8 +44,11 @@ public class Hand_Breaker : MonoBehaviour
         GameObject Parent = other.GetComponent<Collider_Dir>().Parent;
         //if (CheckDirection(other.gameObject, Parent))
         //{
+
         if (Type == Parent.GetComponent<CubesBehaviour>().type)
         {
+            bladeHit.PlayOneShot(bladeHit.clip);
+            boxHit.PlayOneShot(boxHit.clip);
             GM.DestroyCube(Parent);
             GM.AddPunctuation();
         }
---
+++ AlphaBlades/Assets/Scripts/MusicTransition.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicTransition : MonoBehaviour
+{
+    public AudioClip loop_track;
+    AudioSource audioSrc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSrc = gameObject.GetComponent<AudioSource>();
+        audioSrc.loop = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!audioSrc)
+            audioSrc = gameObject.GetComponent<AudioSource>();
+
+        if(!audioSrc.isPlaying)
+        {
+            audioSrc.clip = loop_track;
+            audioSrc.loop = true;
+            audioSrc.Play();
+        }
+    }
+}
